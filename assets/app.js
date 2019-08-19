8fd1a877e35d439ecb0357123984890662f4d639
keenan560 <keenan560@gmail.com>
2019-08-18 21:24:01 -0400

    starter file

---
+++ assets/app.js
@@ -0,0 +1,44 @@
+console.log("connected");
+
+$(document).on("click", "#skills", function(event){
+    event.preventDefault();
+    $("#feed").html(`<div id='skills-bg'><p class='h2 p-2 my-3 skills-header text-white'>HTML: 92%</p>
+    <div class="progress">
+  <div id='html-bar' class="progress-bar progress-bar animated slideInLeft slower bg-danger" role="progressbar" style="width: 92%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+</div><p class='h2 p-2 my-3  skills-header text-white'>CSS: 87%</p> <div class="progress">
+  <div id='css-bar' class="progress-bar progress-bar animated slideInLeft slower bg-danger" role="progressbar" style="width: 87%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div><p class='h2 p-2 my-3  skills-header text-white'>Javascript: 94%</p> <div class="progress">
+  <div id='javascript-bar' class="progress-bar progress-bar animated slideInLeft slower bg-danger" role="progressbar" style="width: 94%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+</div><p class='h2 p-2 my-3  skills-header text-white'>SQL: 92%</p> <div class="progress">
+  <div id='sql-bar' class="progress-bar progress-bar animated slideInLeft slower bg-danger" role="progressbar" style="width: 95%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div></div>`);
+})
+
+$(document).on("click", "#html", function (event) {
+    event.preventDefault();
+    $("#feed").html(`<p class='h1 text-white'>{HTML: 92%}</p>
+    <div class="progress">
+  <div class="progress-bar progress-bar-animated" role="progressbar" style="width: 92%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+</div>`);
+})
+
+$(document).on("click", "#css", function (event) {
+    event.preventDefault();
+    $("#feed").html(`<p class='h1 text-white'>{CSS: 87%}</p> <div class="progress">
+  <div class="progress-bar progress-bar-animated bg-success" role="progressbar" style="width: 87%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>`);
+})
+
+$(document).on("click", "#javascript", function (event) {
+    event.preventDefault();
+    $("#feed").html(`<p class='h1 text-white'>{Javascript: 94%}</p> <div class="progress">
+  <div class="progress-bar progress-bar-animated bg-info" role="progressbar" style="width: 94%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+</div>`);
+})
+
+$(document).on("click", "#sql", function (event) {
+    event.preventDefault();
+    $("#feed").html(`<p class='h1 text-white'>{SQL: 92%}</p> <div class="progress">
+  <div class="progress-bar progress-bar-animated bg-danger" role="progressbar" style="width: 95%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div>`);
+})
