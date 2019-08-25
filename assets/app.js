4d4a9bf6de9396e2f1175e371dfa2756b2556206
keenan560 <keenan560@gmail.com>
2019-08-25 08:36:08 -0400

    updated portfolio page with link

--- assets/app.js
+++ assets/app.js
@@ -17,34 +17,6 @@ $(document).on("click", "#skills", function(event){
 </div></div>`);
 })
 
-// $(document).on("click", "#html", function (event) {
-//     event.preventDefault();
-//     $("#feed").html(`<p class='h1 text-white'>{HTML: 92%}</p>
-//     <div class="progress">
-//   <div class="progress-bar progress-bar-animated" role="progressbar" style="width: 92%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-// </div>`);
-// })
-
-// $(document).on("click", "#css", function (event) {
-//     event.preventDefault();
-//     $("#feed").html(`<p class='h1 text-white'>{CSS: 87%}</p> <div class="progress">
-//   <div class="progress-bar progress-bar-animated bg-success" role="progressbar" style="width: 87%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-// </div>`);
-// })
-
-// $(document).on("click", "#javascript", function (event) {
-//     event.preventDefault();
-//     $("#feed").html(`<p class='h1 text-white'>{Javascript: 94%}</p> <div class="progress">
-//   <div class="progress-bar progress-bar-animated bg-info" role="progressbar" style="width: 94%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-// </div><ul class='text-white'>Libraries<li>jQuery</li><li>NodeJS</li><li>express.js</li><li>express-handlebars</li></ul>`);
-// })
-
-// $(document).on("click", "#sql", function (event) {
-//     event.preventDefault();
-//     $("#feed").html(`<p class='h1 text-white'>{SQL: 92%}</p> <div class="progress">
-//   <div class="progress-bar progress-bar-animated bg-danger" role="progressbar" style="width: 95%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-// </div>`);
-// });
 
 
 $(document).on("click", "#resume", (event) => {
@@ -52,6 +24,6 @@ $(document).on("click", "#resume", (event) => {
   console.log("clicked");
     $('html, body').animate({
       scrollTop: $("#resume-feed").offset().top},'slow');
-  $("#resume-feed").html(`<iframe src='assets/Keenan Mapp Resume (1).pdf'>hello</</iframe>`)
+  $("#resume-feed").html(`<iframe src='./assets/Keenan Mapp Resume (1).pdf'>hello</</iframe>`)
 });
 
