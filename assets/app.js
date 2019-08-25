c5dad05afff48c219fd4153c70e0c754d60aec15
keenan560 <keenan560@gmail.com>
2019-08-25 06:53:41 -0400

    resume added with iframe, added smooth scroll feature

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,9 @@ console.log("connected");
 
 $(document).on("click", "#skills", function(event){
     event.preventDefault();
+  $('html, body').animate({
+    scrollTop: $("#feed").offset().top
+  }, 'slow');
     $("#feed").html(`<div id='skills-bg'><p class='h4 p-2 my-3 skills-header text-white'>HTML: 92%</p>
     <div class="progress">
   <div id='html-bar' class="progress-bar progress-bar animated slideInLeft slower bg-danger" role="progressbar" style="width: 92%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
@@ -14,31 +17,41 @@ $(document).on("click", "#skills", function(event){
 </div></div>`);
 })
 
-$(document).on("click", "#html", function (event) {
-    event.preventDefault();
-    $("#feed").html(`<p class='h1 text-white'>{HTML: 92%}</p>
-    <div class="progress">
-  <div class="progress-bar progress-bar-animated" role="progressbar" style="width: 92%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-</div>`);
-})
+// $(document).on("click", "#html", function (event) {
+//     event.preventDefault();
+//     $("#feed").html(`<p class='h1 text-white'>{HTML: 92%}</p>
+//     <div class="progress">
+//   <div class="progress-bar progress-bar-animated" role="progressbar" style="width: 92%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+// </div>`);
+// })
 
-$(document).on("click", "#css", function (event) {
-    event.preventDefault();
-    $("#feed").html(`<p class='h1 text-white'>{CSS: 87%}</p> <div class="progress">
-  <div class="progress-bar progress-bar-animated bg-success" role="progressbar" style="width: 87%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-</div>`);
-})
+// $(document).on("click", "#css", function (event) {
+//     event.preventDefault();
+//     $("#feed").html(`<p class='h1 text-white'>{CSS: 87%}</p> <div class="progress">
+//   <div class="progress-bar progress-bar-animated bg-success" role="progressbar" style="width: 87%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+// </div>`);
+// })
 
-$(document).on("click", "#javascript", function (event) {
-    event.preventDefault();
-    $("#feed").html(`<p class='h1 text-white'>{Javascript: 94%}</p> <div class="progress">
-  <div class="progress-bar progress-bar-animated bg-info" role="progressbar" style="width: 94%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-</div><ul class='text-white'>Libraries<li>jQuery</li><li>NodeJS</li><li>express.js</li><li>express-handlebars</li></ul>`);
-})
+// $(document).on("click", "#javascript", function (event) {
+//     event.preventDefault();
+//     $("#feed").html(`<p class='h1 text-white'>{Javascript: 94%}</p> <div class="progress">
+//   <div class="progress-bar progress-bar-animated bg-info" role="progressbar" style="width: 94%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+// </div><ul class='text-white'>Libraries<li>jQuery</li><li>NodeJS</li><li>express.js</li><li>express-handlebars</li></ul>`);
+// })
 
-$(document).on("click", "#sql", function (event) {
-    event.preventDefault();
-    $("#feed").html(`<p class='h1 text-white'>{SQL: 92%}</p> <div class="progress">
-  <div class="progress-bar progress-bar-animated bg-danger" role="progressbar" style="width: 95%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-</div>`);
-})
+// $(document).on("click", "#sql", function (event) {
+//     event.preventDefault();
+//     $("#feed").html(`<p class='h1 text-white'>{SQL: 92%}</p> <div class="progress">
+//   <div class="progress-bar progress-bar-animated bg-danger" role="progressbar" style="width: 95%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+// </div>`);
+// });
+
+
+$(document).on("click", "#resume", (event) => {
+  event.preventDefault(); 
+  console.log("clicked");
+    $('html, body').animate({
+      scrollTop: $("#resume-feed").offset().top},'slow');
+  $("#resume-feed").html(`<iframe src='assets/Keenan Mapp Resume (1).pdf'>hello</</iframe>`)
+});
+
